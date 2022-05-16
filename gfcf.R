f9f507c462425b0e64f1ad9116204fb9d3371360
Christian Spence <mail@christianspence.co.uk>
2022-05-16 12:30:57 +0100

    Updated to correct issue with multiple blank rows in one of the original data sheets. Also library calls improved.

--- gfcf.R
+++ gfcf.R
@@ -1,19 +1,25 @@
-library(readxl)
-library(tidyverse)
+# https://www.ons.gov.uk/economy/regionalaccounts/grossdisposablehouseholdincome/articles/experimentalregionalgrossfixedcapitalformationgfcfestimatesbyassettype1997to2020/2022-05-10
 
-gfcf_path <- "experimentalregionalgfcf19972020byassetandindustry.xlsx"
+gfcf_path <- "data-raw/experimentalregionalgfcf19972020byassetandindustry.xlsx"
 
-gfcf_sheets <- excel_sheets(gfcf_path)
+gfcf_sheets <- readxl::excel_sheets(gfcf_path)
 gfcf_data_sheets <- gfcf_sheets[grepl("[0-9].[0-9]", gfcf_sheets)]
 
 gfcf <- lapply(gfcf_data_sheets, function(sht) {
-  read_excel(gfcf_path, sheet = sht, skip = 3, na = c("[w]", "[low]")) |>
-    pivot_longer(cols = `1997`:`2020`, names_to = "date")
+  readxl::read_excel(gfcf_path,
+                     sheet = sht,
+                     skip = 3,
+                     na = c("[w]", "[low]")) |>
+    tidyr::pivot_longer(cols = `1997`:`2020`, names_to = "date") |>
+    dplyr::filter(!is.na(Asset)) # Remove empty rows from data sheet 2
 })
 
 gfcf_final <- lapply(gfcf, function(x) {
-  max_itl <- max(str_sub(names(x)[grepl("ITL[0-9]", names(x))], 4, 4))
-  select(x, date, contains(paste0("ITL", max_itl)), contains("SIC07"), Asset, value) |>
-    rename(geog_name = 2, geog_code = 3)
+  max_itl <- max(stringr::str_sub(names(x)[grepl("ITL[0-9]", names(x))], 4, 4))
+  dplyr::select(x,
+                date,
+                dplyr::contains(paste0("ITL", max_itl)),
+                dplyr::contains("SIC07"), Asset, value) |>
+    dplyr::rename(geog_name = 2, geog_code = 3)
 }) |>
-  bind_rows()
+  dplyr::bind_rows()
